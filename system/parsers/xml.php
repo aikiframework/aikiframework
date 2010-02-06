55fdb3ce68e738d39818c0acd2bf31979a3b1360
Bassel Safadi <bassel@bassel-laptop>
2010-02-06 19:35:18 +0200

    rss reader output

--- system/parsers/xml.php
+++ system/parsers/xml.php
@@ -13,29 +13,85 @@
 
 class aiki_xml
 {
+	var $timeout = 5; // set to zero for no timeout
+
 	function rss_parser($text){
 		global $aiki;
 
-		$rss_matchs = preg_match_all('/\<rss (.*) rss\>/Us', $text, $matchs);
+		$rss_matchs = preg_match_all('/\<rss\>(.*)\<\/rss\>/Us', $text, $matchs);
 
 		if ($rss_matchs > 0){
 
 			foreach ($matchs[1] as $rss){
 
-				$rss_url = trim($rss);
-				$content = file_get_contents($rss_url );
+				$rss_url = $aiki->get_string_between($rss , "<url>", "</url>");
+				$rss_url = trim($rss_url);
 
-				if ($content !== false) {
-					$output = '';
-					$xml = new SimpleXMLElement($content);
+				$limit = $aiki->get_string_between($rss , "<limit>", "</limit>");
+				$limit = trim($limit);
 
-					foreach ($xml->channel->item as $item) {
-						$output .= "<a href='$item->link'>".$item->title.'</a><br />';
-						$output .= $item->description.'<br />';
-					}
+				$output = $aiki->get_string_between($rss , "<output>", "</output>");
+
+				if(!$output){
+
+					$output = "<div class='news'>
+						<h4>[[title]]</h4>
+						<p>[[pubDate]]</p>
+						<p><a href='[[link]]'>[[guid]]</a></p>
+						<div class='description'>[[description]]</div>
+						</div>";
 				}
 
-				$text = str_replace("<rss $rss rss>", $output , $text);
+				$ch = curl_init();
+				curl_setopt ($ch, CURLOPT_URL, $rss_url);
+				curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+
+				ob_start();
+				curl_exec($ch);
+				curl_close($ch);
+				$content = ob_get_contents();
+				ob_end_clean();
+
+
+				if ($content !== false) {
+
+					$xml = new SimpleXMLElement($content);
+
+					$i = 1;
+
+					$html_output = '';
+
+					foreach ($xml->channel->item as $item) {
+
+						$items_matchs = preg_match_all('/\[\[(.*)\]\]/Us', $output, $elements);
+
+						if ($items_matchs > 0){
+								
+							$processed_output = $output;
+
+							foreach ($elements[1] as $element){
+
+								$element = trim($element);
+
+								$processed_output = str_replace("[[".$element."]]", $item->$element, $processed_output);
+
+							}
+
+							$html_output .= $processed_output;
+							$processed_output = '';
+						}
+
+
+
+						if (isset($limit) and $limit == $i){
+							break;
+						}
+						$i++;
+					}
+
+				}
+
+				$text = str_replace("<rss>$rss</rss>", $html_output , $text);
 			}
 		}
 
