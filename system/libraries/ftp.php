15933cad51a43ef1bba546388e6ca93df990bb52
Bassel Safadi <bassel@bassel-laptop>
2010-02-09 15:54:00 +0200

    changed license to agpl

--- system/libraries/ftp.php
+++ system/libraries/ftp.php
@@ -1,13 +1,12 @@
 <?php
 
 /*
- * Aikiframework
+ * Aiki framework (PHP)
  *
- * @author		Bassel Khartabil
- * @copyright	Copyright (C) 2009-2010 Aikilab inc
- * @license		http://www.gnu.org/licenses/gpl.html
+ * @author		http://www.aikilab.com
+ * @license		http://www.fsf.org/licensing/licenses/agpl-3.0.html
  * @link		http://www.aikiframework.org
- */ 
+ */
 
 class aiki_ftp
 {
