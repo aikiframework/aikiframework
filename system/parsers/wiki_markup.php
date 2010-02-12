74adccf127a55bfcf761accbf9753e74eae59a00
Bassel Safadi <bassel@bassel-laptop>
2010-02-12 13:03:30 +0200

    markup_tables is now part of wiki parser

--- system/parsers/wiki_markup.php
+++ system/parsers/wiki_markup.php
@@ -14,6 +14,52 @@ if(!defined('IN_AIKI')){die('No direct script access allowed');}
 class aiki_wiki_markup
 {
 
+	function markup_tables($text){
+
+		global $aiki;
+		//TODO colspan and cellspan and table header and footer and colored columns
+
+		$numMatches = preg_match_all( '/\{\|/', $text, $matches);
+		for ($i=0; $i<$numMatches; $i++){
+
+			$get_table_contents = $this->get_string_between($text, "{|", "|}");
+
+			$table_atr = $aiki->get_string_between($text, "{|", "|-");
+			$table_atr = trim($table_atr);
+			$table_atr = str_replace("<br>", "", $table_atr);
+			$table_atr = str_replace("<br />", "", $table_atr);
+
+			$table = "<table $table_atr >";
+
+
+			$get_rows = explode("|-", $get_table_contents);
+			unset($get_rows[0]); //fix for the first element being empty
+			foreach ($get_rows as $row){
+				$table .= "<tr>";
+
+				$get_cells = explode("|", $row);
+				unset($get_cells[0]); //fix for the first element being empty
+
+
+				foreach ($get_cells as $cell){
+					$table .= "<td>$cell</td>";
+
+				}
+
+				$table .= "</tr>";
+			}
+
+
+			$table .= "</table>";
+
+			$text = str_replace ("{|".$get_table_contents."|}", $table, $text);
+		}
+
+
+		return $text;
+
+	}
+
 	//From mediawiki 1.14.0 Parser
 	function markup_quotes( $text ) {
 		$arr = preg_split( "/(''+)/", $text, -1, PREG_SPLIT_DELIM_CAPTURE );
