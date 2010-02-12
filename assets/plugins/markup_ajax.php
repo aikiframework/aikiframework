21c9161b8e8f5488deb8aaf4e70b3483c7cf396a
Bassel Safadi <bassel@bassel-laptop>
2010-02-12 13:10:43 +0200

    markup ajax bug fix

---
+++ assets/plugins/markup_ajax.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * Aiki framework (PHP)
+ *
+ * @author		http://www.aikilab.com
+ * @license		http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @link		http://www.aikiframework.org
+ */
+
+if(!defined('IN_AIKI')){die('No direct script access allowed');}
+
+
+class markup_ajax extends aiki
+{
+
+	function markup_ajax($text){
+		global $db;
+		/*
+		 <script type="text/javascript">
+		 $(document).ready(function(){
+		 function ajaxSOMETHING(file, targetwidget){
+		 $.get('http://www.aikiframework.org/aikidev/admin_tools/edit/widgets/'+file,function(data) {
+		 $(targetwidget).html(data);
+		 });
+		 }
+
+		 $("a").click(function(event){
+		 globalajaxify($(this).attr("rel"), $(this).attr("href"));
+		 return false;
+
+		 });
+		 });
+		 </script>
+		 */
+
+		$count_links = preg_match_all('/\(ajax\_a\((.*)\)ajax\_a\)/Us', $text, $links);
+
+		if ($count_links > 0){
+
+
+			foreach ($links[1] as $set_of_requests)
+			{
+				$output = '';
+
+				$array = explode(';', $set_of_requests);
+
+				$array_of_values = $array;
+
+				unset($array_of_values[0]);
+
+				$function_name = str_replace('-', '', $array[0]);
+
+				$output .= " <script type=\"text/javascript\">
+				$(document).ready(function(){
+				function $function_name(file, targetwidget, callback){
+
+				$(targetwidget).load(file, {limit: 25}, function(){
+				eval(callback);
+			});
+			}
+		 $(\"#$array[0]\").click(function(event){
+		 ";
+
+				foreach ($array_of_values as $value){
+
+					$value = $this->get_string_between($value, "[", "]");
+
+					$value = explode(',', $value);
+
+					$url = $this->get_string_between($value['0'], "'", "'");
+					$target = $this->get_string_between($value['1'], "'", "'");
+					
+					if (isset ($value['2'])){
+						$callback = $this->get_string_between($value['2'], "'", "'");
+					}
+
+					$output .= "$function_name('$url', '$target'";
+
+					if ($callback){
+						$output .= ", '$callback;'";
+					}
+
+					$output .= ");"."\n";
+
+				}
+
+
+
+				$output .= "return false;
+
+		 });
+		 });
+		 </script>";
+
+				$text = preg_replace('/\(ajax\_a\('.preg_quote($set_of_requests, '/').'\)ajax\_a\)/Us', $output, $text);
+
+			}
+
+		}
+
+		return $text;
+	}
+}
+?>
