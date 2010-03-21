6f4ccea398f7fd51354b7da1490bbb6146158e47
Bassel Safadi <bassel@bassel-laptop>
2010-03-22 01:00:11 +0200

    replaced complicated removeXSS function with simple htmlspecialchars

--- system/libraries/security.php
+++ system/libraries/security.php
@@ -49,42 +49,9 @@ class security
 
 
 	public function RemoveXSS($val) {
-		$val = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $val);
-		$search = 'abcdefghijklmnopqrstuvwxyz';
-		$search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		$search .= '1234567890!@#$%^&*()';
-		$search .= '~`";:?+/={}[]-_|\'\\';
-		for ($i = 0; $i < strlen($search); $i++) {
-			$val = preg_replace('/(&#[x|X]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val); // with a ;
-			$val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val); // with a ;
-		}
 
-		$ra1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
-		$ra2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
-		$ra = array_merge($ra1, $ra2);
+		$val = htmlspecialchars($val);
 
-		$found = true;
-		while ($found == true) {
-			$val_before = $val;
-			for ($i = 0; $i < sizeof($ra); $i++) {
-				$pattern = '/';
-				for ($j = 0; $j < strlen($ra[$i]); $j++) {
-					if ($j > 0) {
-						$pattern .= '(';
-						$pattern .= '(&#[x|X]0{0,8}([9][a][b]);?)?';
-						$pattern .= '|(&#0{0,8}([9][10][13]);?)?';
-						$pattern .= ')?';
-					}
-					$pattern .= $ra[$i][$j];
-				}
-				$pattern .= '/i';
-				$replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2); // add in <> to nerf the tag
-				$val = preg_replace($pattern, $replacement, $val); // filter out the hex tags
-				if ($val_before == $val) {
-					$found = false;
-				}
-			}
-		}
 		return $val;
 	}
 
