b075297be8c9fac8e8017fd506c3a52961bb0c1a
bassel <bassel@bassel-laptop>
2010-10-15 22:10:19 +0300

    updated jquery forms to version  2.47

--- assets/apps/admin/control_panel.js
+++ assets/apps/admin/control_panel.js
@@ -333,31 +333,10 @@ function urls_widgets_tree(){
 				
 				$('<input type="hidden" name="edit_form" value="Save">').appendTo("#edit_form");
 				$(window).keypress(function(event) {
-				    if (!(event.which == 115 && event.ctrlKey)) return true;
-				    $("#edit_form").ajaxSubmit(
-				    		function() { 
-								stop = 0;
-								$('<div id="note_container" style="background:none repeat scroll 0 0 #FDA501; color:#FFFFFF; font-weight:bold; padding:6px; position:fixed; right:0; text-align:center; top:0; width:100%; z-index:10000;"><span>Changes Saved</span></div>').appendTo("#widget-form").hide().fadeIn(1000).fadeOut(2000,
-										function() {
-									
-									aiki_log('Widget: You changed widget '+NODE.id);
-									
-								        	$('#note_container').remove();	
-											$("<div id='events_listener'></div>").appendTo("#widget-form");
-											$("#events_listener").load("assets/apps/admin/events.php?saved=true&widget="+NODE.id);
-											$("#events_listener").remove();					        	
-								        }
-								);
-
-								
-								$.tree_reference('widgettree').refresh();
-								
-								stop = 1;
-				    		}); 
-				    event.preventDefault();
-				    return false;
-				});
-				
+				    if (event.which == 115 && event.ctrlKey){
+				    	 event.preventDefault();
+				     }
+			    });
 
 			});	
   }
