2d3f81a76a518929f7414283671bce00e576c468
Bassel Safadi <b@ssel.me>
2010-11-12 18:59:38 +0200

    replaced the dotes with || in inline permissions

--- src/system/libraries/security.php
+++ src/system/libraries/security.php
@@ -49,14 +49,14 @@ class security
 		$inline = preg_match_all('/\(\#\(permissions\:(.*)\)\#\)/Us', $text, $matchs);
 		if ($inline > 0){
 			foreach ($matchs[1] as $inline_per){
-				$get_sides = explode(":", $inline_per);
+				$get_sides = explode("||", $inline_per);
 
 				$get_group_level = $db->get_var ("SELECT group_level from aiki_users_groups where group_permissions='$get_sides[0]'");
 
 				if ($get_sides[0] == $membership->permissions or $membership->group_level < $get_group_level){
-					$text = str_replace("(#(permissions:$get_sides[0]:$get_sides[1])#)", $get_sides[1], $text);
+					$text = str_replace("(#(permissions:$get_sides[0]||$get_sides[1])#)", $get_sides[1], $text);
 				}else{
-					$text = str_replace("(#(permissions:$get_sides[0]:$get_sides[1])#)", '', $text);
+					$text = str_replace("(#(permissions:$get_sides[0]||$get_sides[1])#)", '', $text);
 				}
 
 			}
