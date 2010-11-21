ae50c5485127e947848f6e051447eaf96d064dbf
Bassel Safadi <b@ssel.me>
2010-11-22 00:20:27 +0200

    imporved get_string_between function, by Roger Mart&#237;n

--- src/system/core.php
+++ src/system/core.php
@@ -110,9 +110,8 @@ class aiki
 	 * @return  string
 	 */
 	public function get_string_between($string, $start, $end){
-		$string = " ".$string;
 		$ini = strpos($string,$start);
-		if ($ini == 0) return "";
+		if ($ini ===false) return "";
 		$ini += strlen($start);
 		$len = strpos($string,$end,$ini) - $ini;
 		return substr($string,$ini,$len);
