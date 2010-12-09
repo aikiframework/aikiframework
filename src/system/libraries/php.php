9c6dc613b9fb7c9bba3dc68005a4b094f18d7beb
Bassel Safadi <b@ssel.me>
2010-12-09 16:23:47 +0200

    Roger Mart&#237;n rewrote php parser, huge performance improvements

--- src/system/libraries/php.php
+++ src/system/libraries/php.php
@@ -26,194 +26,110 @@ if(!defined('IN_AIKI')){die('No direct script access allowed');}
  * @category    Aiki
  * @package     Library
  */
+
 class php
 {
 
 	public function parser($text){
-		global $aiki;
+		global $aiki; //put global $hi to test dump.
 
-		if (!preg_match ("/\<form(.*)\<php (.*) php\>(.*)\<\/form\>/Us", $text)){
+		$para="([^,]*)"; // for comodity
 
-			$php_matchs = preg_match_all('/\<php (.*) php\>/Us', $text, $matchs);
-
-		}else{
-
-			$php_matchs = 0;
+		if (preg_match ("/\<form(.*)\<php (.*) php\>(.*)\<\/form\>/Us", $text)){
+			return $text;
 		}
 
-		if ($php_matchs > 0){
+		if (preg_match_all('/\<php (.*) php\>/Us', $text, $matchs)){
 
 			foreach ($matchs[1] as $php_function){
 
-				if (preg_match('/eval((.*));/Us', $php_function)){
-					$php_output = $this->aiki_eval('<php '.$php_function.' php>');
-				}else{
-	
-					if (preg_match('/str_replace((.*));/Us', $php_function)){
-						$php_output = $this->aiki_str_replace($php_function);
-					}
-	
-					if (preg_match('/substr((.*));/Us', $php_function)){
-						$php_output = $this->aiki_substr($php_function);
-					}
-	
-					if (preg_match('/if(.*) then (.*)/Us', $php_function)){
-						$php_output = $this->aiki_if_then($php_function);
-					}
-	
-					if (preg_match('/htmlspecialchars\((.*)\)/Us', $php_function)){
-						$php_output = $this->aiki_htmlspecialchars($php_function);
-					}
-	
-					if (preg_match('/\$aiki\-\>(.*)\-\>(.*)\((.*)\)\;/Us', $php_function)){
-						$php_output = $this->aiki_function($php_function);
-					}
+				$php_output="";
+
+				// obtain first word..
+				$len= strcspn($php_function," -(");
+				$word = ( $len ? substr($php_function,0,$len): "");
+
+				//evaluate each case..
+				switch ($word) {
+					/*case "dump":
+						preg_match('/dump (.*);/s', $php_function, $partial);
+						ob_start();
+						eval( "return var_dump({$partial[1]});");
+						$php_output= ob_get_clean();
+						break;
+
+					   case "echo":
+						preg_match('/echo (.*);/s', $php_function, $partial);
+						$php_output = eval( "return {$partial[1]};") ;
+						break;
+
+					case "eval":
+						preg_match('/eval\((.*)\);/s', $php_function, $partial);
+						$php_output = eval($partial[1] . ( substr($partial[1],-1)!=";" ? ";" :"")) ;
+						break;*/
+
+					case "str_replace":
+						preg_match("/str_replace\($para,$para,$para\);/Us", $php_function, $partial);
+						$php_output = str_replace($partial[1],$partial[2],$partial[3]);
+						break;
+
+					case "substr":
+						preg_match("/substr\($para,$para(,(.*))?\);/s", $php_function, $partial);
+						$php_output = ( $partial[4] ? substr($partial[1], $partial[2],$partial[4])
+						: substr($partial[1], $partial[2] ));
+						break;
+
+					case "if":
+						if (preg_match('/if (.*) then (.*) else (.*);/s', $php_function, $partial)){
+							$php_output=  ( $partial[1] ? $partial[2]: $partial[3]);
+						} elseif (preg_match('/if (.*) then (.*);/s', $php_function, $partial)){
+							$php_output=  ( $partial[1] ? $partial[2]:"");
+						}
+						break;
+
+					case "htmlspecialchars":
+						preg_match('/htmlspecialchars\((.*)\);/s', $php_function, $partial);
+						$php_output = htmlspecialchars($partial[1]);
+						break;
+
+					case '$aiki':
+						preg_match('/\$aiki\-\>(.*)\-\>(.*)\((.*)\)\;/s', $php_function,$partial);
+						$php_output = $this->aiki_function($partial[1],$partial[2],$partial[3]);
+						break;
+
+					default :
+						if ( isset($config['debug']) or true){
+							$php_output = "<php $php_function php>";
+						}
+
+
 				}
 
 				$text = str_replace("<php $php_function php>", $php_output , $text);
 			}
 		}
-
 		return $text;
+
 	}
 
-	/**
-	 * Evaluate a string as PHP code.
-	 * 
-	 * @param   string $text The complete PHP statement such as: eval(echo "hello";);
-	 * @return  mixed $result Returns NULL unless return is called in the evaluated code, in which case the value passed to return is returned. If there is a parse error in the evaluated code, eval() returns FALSE and execution of the following code continues normally
-	 */
-	public function aiki_eval($text){
+
+
+	public function aiki_function($class,$function, $para){
 		global $aiki;
 
-		$code = $aiki->get_string_between($text, '<php eval(', '); php>');
-
-		$result = eval($code);
-			
-		return $result;
-	}
-
-	public function aiki_function($text){
-		global $aiki;
-
-		//function does not have vars
-		if (preg_match('/\$aiki\-\>(.*)\-\>(.*)\(\)\;/Us', $text)){
-
-			$class = $aiki->get_string_between($text, '$aiki->', '->');
-			$function = $aiki->get_string_between($text, '$aiki->'.$class.'->', '();');
-
-			if (isset($aiki->$class)){
-				$output = $aiki->$class->$function();
-			}else{
-				//try to load the library
-				$aiki->load($class);
-
-				if (isset($aiki->$class)){
-					$output = $aiki->$class->$function();
-				}else{
-					$output = '';
-				}
-			}
-
-			//function has vars
-		}elseif (preg_match('/\$aiki\-\>(.*)\-\>(.*)\((.*)\)\;/Us', $text)){
-
-			$class = $aiki->get_string_between($text, '$aiki->', '->');
-			$function = $aiki->get_string_between($text, '$aiki->'.$class.'->', '(');
-
-			$vars_array = preg_match('/'.$function.'\((.*)\)\;$/Us', $text, $vars_match);
-			if ($vars_match[1]){
-				$vars_array = $vars_match[1];
-			}else{
-				$vars_array = '';
-			}
-
-			if (isset($aiki->$class)){
-				$output = $aiki->$class->$function($vars_array);
-
-			}else{
-				//try to load the library
-				$aiki->load($class);
-
-				if (isset($aiki->$class)){
-					$output = $aiki->$class->$function($vars_array);
-				}else{
-					$output = '';
-				}
-
-			}
-
+		// load class if not exists..
+		if (!isset($aiki->$class)){
+			$aiki->load($class);
 		}
 
-		return $output;
-	}
-
-	public function aiki_htmlspecialchars($text){
-		global $aiki;
-
-		$original_string = $aiki->get_string_between($text, "htmlspecialchars(", ")");
-
-		$string = htmlspecialchars($original_string);
-
-		$text = str_replace("htmlspecialchars($original_string)", $string, $text);
-			
-		return $text;
-	}
-
-
-	public function aiki_if_then($text){
-		global $aiki;
-
-		$string = explode(" then ", $text);
-		$if_cond = explode('=', $string[0]);
-
-		$if_cond[0] = str_replace("if ", "", $if_cond[0]);
-		$if_cond[0] = trim($if_cond[0]);
-
-		if (!isset($if_cond[1])){
-			if ($if_cond[0]){
-				$output = trim($string[1]);
-			}else{
-				$output = '';
-			}
-		}else{
-
-			$if_cond[1] = trim($if_cond[1]);
-
-			if ($if_cond[0] == $if_cond[1]){
-				$output = trim($string[1]);
-			}else{
-				$output = '';
-			}
+		if ($para){
+			return  $aiki->$class->$function($para);
+		} else {
+			return $aiki->$class->$function();
 		}
 
-		return $output;
+
 	}
 
-	public function aiki_str_replace($text){
-		global $aiki;
-
-		$string = $aiki->get_string_between($text, "str_replace(", ");");
-		$string = explode(",", $string );
-
-		$output = str_replace($string[0], $string[1] , $string[2]);
-			
-		return $output;
-	}
-
-	public function aiki_substr($text){
-		global $aiki;
-
-		$string = $aiki->get_string_between($text, "substr(", ");");
-		$string = explode(",", $string );
-
-		if ($string[2]){
-			$output = substr($string[0], $string[1] , $string[2]);
-		}else{
-			$output = substr($string[0], $string[1]);
-		}
-			
-		return $output;
-	}
 
 }
