3534a68ec2729a7b28e55c41230aca54004ccf47
Bassel Safadi <b@ssel.me>
2010-12-28 19:36:43 +0200

    added basic captcha functions

--- src/system/libraries/captcha.php
+++ src/system/libraries/captcha.php
@@ -28,6 +28,35 @@ if(!defined('IN_AIKI')){die('No direct script access allowed');}
  */
 class captcha
 {
-	
-	
+
+	public function create_randome_text(){
+
+		$md5 = md5(microtime() * mktime());
+
+		$string = substr($md5,0,5);
+
+		return $string;
+	}
+
+
+	public function write_text_to_image(){
+
+		$string = $this->create_randome_text();
+
+		$captcha = imagecreatefrompng("./captcha.png");
+
+		$black = imagecolorallocate($captcha, 0, 0, 0);
+		$line = imagecolorallocate($captcha,233,239,239);
+		imageline($captcha,0,0,39,29,$line);
+		imageline($captcha,40,0,64,29,$line);
+
+		imagestring($captcha, 5, 20, 10, $string, $black);
+
+		$_SESSION['captcha_key'] = md5($string);
+
+		header("Content-type: image/png");
+		imagepng($captcha);
+
+	}
+
 }
