b968abaa2461571d09ab134500f70dd75bb14b29
Bassel Safadi <b@ssel.me>
2011-01-02 13:55:40 +0200

    added ShowTableStructure function to aiki bot

--- src/system/libraries/bot.php
+++ src/system/libraries/bot.php
@@ -331,6 +331,28 @@ class bot
 
 	}
 
+	function ShowTableStructure($table){
+
+		$output = '<h3>'.$table.'</h3>';
+		$result2 = mysql_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
+		if(mysql_num_rows($result2)) {
+			echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+			echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
+			while($row2 = mysql_fetch_row($result2)) {
+				$output .= '<tr>';
+				foreach($row2 as $key=>$value) {
+					$output .= '<td>'.$value.'</td>';
+				}
+				$output .= '</tr>';
+			}
+			$output .= '</table>';
+
+			return $output;
+		}
+	}
+
+
+
 	function DataGrid($form_id){
 		global $db, $config, $aiki;
 
