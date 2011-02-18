691ad03c36390dea5678482ba8f5eafa8811b820
Bassel Safadi <b@ssel.me>
2011-02-18 20:21:38 +0200

    fixed Bug #714433 is_valid doesnot accept alias+email@somewhere.com

--- src/system/libraries/text.php
+++ src/system/libraries/text.php
@@ -69,7 +69,7 @@ class text
 		$valid = false;
 		switch ($type) {
 			case "email":
-				if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $var)) {
+				if (preg_match("/^[_a-z0-9-+]+(\.[_a-z0-9-+]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/i", $var)) {
 					$valid = true;
 				}
 				break;
