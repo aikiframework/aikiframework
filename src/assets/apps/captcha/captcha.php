7ead63fe34dfe2b4d00793c49c395754d8a3d6ad
Bassel Safadi <b@ssel.me>
2011-02-21 03:29:38 +0200

    removed captcha library and movde code to captcha app

--- src/assets/apps/captcha/captcha.php
+++ src/assets/apps/captcha/captcha.php
@@ -21,6 +21,31 @@ error_reporting(0);
 
 require_once("../../../aiki.php");
 
-$aiki->load("captcha");
 
-echo $aiki->captcha->write_text_to_image();
+function create_random_text(){
+
+	$md5 = md5(microtime() * mktime());
+	$randnumber = rand(2, 6);
+	$string = substr($md5,$randnumber,6);
+
+	return $string;
+}
+
+
+$string = create_random_text();
+
+$captcha = imagecreatefrompng("./captcha.png");
+
+$black = imagecolorallocate($captcha, 0, 0, 0);
+$line = imagecolorallocate($captcha,233,239,239);
+imageline($captcha,0,0,39,29,$line);
+imageline($captcha,40,0,64,29,$line);
+imageline($captcha,29,64,0,29,$line);
+imageline($captcha,29,64,120,0,$line);
+imagestring($captcha, 5, 20, 5, $string, $black);
+
+$_SESSION['captcha_key'] = md5($string);
+
+header("Content-type: image/png");
+imagepng($captcha);
+
