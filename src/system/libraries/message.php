fc6fd7285bb9735dba6b2f3c41f44c2d3edbe82c
Roger Martin <rg1024@gmail.com>
2011-06-07 22:52:37 +0200

    added set,get,show, order,last, first to message class

--- src/system/libraries/message.php
+++ src/system/libraries/message.php
@@ -50,10 +50,10 @@ if(!defined('IN_AIKI')){die('No direct script access allowed');}
 /*
  * function message
  *
- * draft horse of module. Show o return a message.
+ * draft horse of module. Show or return a message.
  *
  * @param $text : text to show. Will be translated if there is a
- *                 t (translate) function or is workin $aiki->languages
+ *                 t (translate) function or is working $aiki->languages
  * @param $attribs (optional=NULL): array of html atributes (id,class, style,onmouse)..
  * @param echo  ($option=false) true, echo the result, and false return it.
  */
@@ -98,23 +98,32 @@ class message {
 
     function store($message, $key="", $add=true){
         if ( $key==""){
-            $this->stored[]= $message;
-        } else {
-            $previuos = ( $add && isset($this->stored[$key])? $this->stored[$key] :"");
-            $this->stored[$key] = $previuos . $message;
-        }        
+            $this->stored[]= array($message);
+            return end($this->stored);
+        } elseif ( $add===false ) {
+            $this->stored[$key]= array($message);
+            return $key;
+        } elseif ( $add===true ) {
+            $this->stored[$key][]= $message;
+            return $key;
+         } else {
+            $this->stored[$key][$add]= $message;
+        }
     }
 
-    function unstore( $key="*", $echo = false, $clear= true){
+    function unstore( $key="*", $echo = false, $clear= true, $glue="\n"){
         $ret="";
         if ( $key=="*" ) {
-           $ret= implode("\n",$this->stored);
+           $ret="";
+           foreach ( $this->stored as $k=>$v){
+                $ret.= implode($glue,$v);
+           }
            if ( $clear) {
                $this->stored= array();
            }
         } else {
             if ( isset($this->stored[$key]) ) {
-                $ret= $this->stored[$key];
+                $ret= implode($glue,$this->stored[$key]);
                 if ( $clear ){
                    unset($this->stored[$key]);
                 }
@@ -126,6 +135,78 @@ class message {
         return $ret;
     }
 
+
+    function order($key, $order="asc"){
+        if ( !isset($this->stored[$key]) ) {
+            return false;
+        }
+        switch ( strtolower($order) ){
+            case "asc"         : ksort($this->stored[$key])  ; break;
+            case "desc"        : krsort($this->stored[$key]) ; break;
+            case "asc-values"  : asort($this->stored[$key])  ; break;
+            case "desc-values" : arsort($this->stored[$key]) ; break;
+            case "natural"     : natsort($this->stored[$key]); break;
+            default : return false;
+        }
+        return true;
+    }
+
+    function set($key,$message){
+        return $this->store($message,$key, false);
+    }
+
+    function add($key,$message,$order=false){
+        return $this->store($message,$key, ( $order===false ? true: $order) );
+    }
+
+    function get($key, $glue="ul" ){
+        if ( !isset($this->stored[$key]) ){
+          return ( $glue=="as array"? array() : "" );
+        }
+
+        $tag= array();;
+        if ( preg_match('#^<([^ />]*) ?[^>]*>$#s',$glue,$tag) ){
+            // it's a HTML tag
+            $tag= $tag[1];
+            echo "*** $tag ***";
+            switch ( $tag ){
+                case "ul":
+                case "ol": return "$glue<li>". implode("</li><li>", $this->stored[$key]) ."</li></$tag>";
+                case "br": return implode($glue, $this->stored[$key]);
+                default  : return "$glue". implode("</$tag>$glue", $this->stored[$key]) ."</$tag>";
+            }
+        }
+
+        switch ($glue){
+            case "as array": return $this->stored[$key];
+            case "comented-list": return "<!-- ". implode(", ", $this->stored[$key]) ."-->";
+            case "li": return "<li>". implode("</li><li>", $this->stored[$key]) ."</li>";
+            case "ol":
+            case "ul": return "<$glue><li>". implode("</li><li>", $this->stored[$key]) ."</li></$glue>";
+            default  : return implode($glue, $this->stored[$key]);
+        }
+        return "";
+    }
+
+    function show($key, $glue){
+        echo $this->get($key,$glue);
+    }
+
+    function last($key){
+        if ( isset($this->stored[$key]) ){
+            return end($this->stored[$key]);
+        }
+        return false;
+    }
+
+    function first($key){
+        if ( isset($this->stored[$key]) ){
+            return reset($this->stored[$key]);
+        }
+        return false;
+    }
+
+
     //shortcuts for login error
     function set_login_error($error){
         $this->store( $this->error($error, array("id"=>"login-wrong-username"),false),"error-in-login");
@@ -134,7 +215,6 @@ class message {
     function get_login_error(){
         return $this->unstore( "error-in-login",false,false);
     }
-
 
     private function Addclass ( $default, &$attribs ){
         return $default . (isset($attribs['class']) ? " ". $attribs['class'] : "" );
