cff2ee3bb66519fa2b70c71ccbca2989b1bb25df
Jon Phillips <jon@rejon.org>
2011-06-10 01:25:24 -0500

    Comments like // don't work with phpdoc, so replaced and made all config options as phpdoc for our code generation, and for knowing what things do!

--- src/configs/config.php
+++ src/configs/config.php
@@ -29,117 +29,228 @@ if (file_exists("$system_folder/configs/aiki-defs.php")){
  */
 $config = array();
 
-// set the database type such as mysql
+/**
+ * Set the database type such as mysql
+ * @global string $config["db_type"] 
+ */
 $config["db_type"] = DB_TYPE;
-// set the database name
+/**
+ * Set the database name
+ * @global string $config["db_name"] 
+ */
 $config["db_name"] = DB_NAME;
-// set the database user name
+/** 
+ * Set the database user name
+ * @global string $config["db_user"] 
+ */
 $config["db_user"] = DB_USER;
-// set the database user password
+/** 
+ * Set the database user password
+ * @global string $config["db_pass"] 
+ */
 $config["db_pass"] = DB_PASS;
-// set the database host server
+/**
+ * Set the database host server
+ * @global string $config["db_host"] 
+ */
 $config["db_host"] = DB_HOST;
-// set the database encoding such as utf8
+/**
+ * Set the database encoding such as utf8
+ * @global string $config["db_encoding"] 
+ */
 $config["db_encoding"] = DB_ENCODE;
-// enable use of mysql set charset
+/**
+ * Enable use of mysql set charset
+ * @global bool $config["db_use_mysql_set_charset"] 
+ */
 $config["db_use_mysql_set_charset"] = false;
 
-// set the full path for SQLite and sqlite PDO
+/**
+ * Set the full path for SQLite and sqlite PDO
+ * @global string $config["db_path"] 
+ */
 $config["db_path"] = "";
 
-// sqlite PDO only
+/**
+ * sqlite PDO only
+ * @global string $config["db_dsn"] 
+ */
 $config["db_dsn"] = "";
 
-// set time out for deleting db cached queries - in hours
+/**
+ * Set time out for deleting db cached queries - in hours
+ * @global integer $config["db_cache_timeout"] 
+ */
 $config["db_cache_timeout"] = 24;
 
-// db caching directory
+/** 
+ * db caching directory
+ * @global string $config["cache_dir"] 
+ */
 $config["cache_dir"] = "cache";
 
-// if set to true will cache the results of sql queries to files
+/** 
+ * if set to true will cache the results of sql queries to files
+ * @global bool $config["enable_query_cache"] 
+ */
 $config["enable_query_cache"] = false;
 
-// enable multi databases connections
+/**
+ * enable multi databases connections
+ * @global bool $config["allow_multiple_databases"] 
+ */
 $config["allow_multiple_databases"] = false;
 
-// use html tidy php extension to format the html output
+/** 
+ * use html tidy php extension to format the html output
+ * @global bool $config["html_tidy"] 
+ */ 
 $config["html_tidy"] = false;
-// enable html tidy compression
+/**
+ * enable html tidy compression
+ * @global bool $config["tidy_compress"] 
+ */
 $config["tidy_compress"] = false;
-// html tidy default configuration
+/**
+ * html tidy default configuration
+ * @global array $config["html_tidy_config"] 
+ */
 $config["html_tidy_config"] = array(
  'indent'       => true,
  'output-xhtml' => true,
  'wrap'         => '0',
 );
 
-// remove empty spaces and lines and have the whole html on one line
+/**
+ * Remove empty spaces and lines and have the whole html on one line
+ * @global bool $config["compress_output"] 
+ */
 $config["compress_output"] = false;
 
-// cache each widget individually in its own file
+/**
+ * Cache each widget individually in its own file
+ * @global bool $config["widget_cache"] 
+ */
 $config["widget_cache"] = false;
 
-// full path to widgets cache directory in case $config["widget_cache"] was true 
+/** 
+ * Full path to widgets cache directory in case $config["widget_cache"] 
+ * was true 
+ * @global string $config["widget_cache_dir"] 
+ */
 $config["widget_cache_dir"] = "";
 
-// enable caching of css
+/** 
+ * Enable caching of css
+ * @global bool $config["css_cache"] 
+ */
 $config["css_cache"] = true;
-// css cache timeout
+/**
+ * CSS cache timeout
+ * @global integer $config["css_cache_timeout"] 
+ */
 $config["css_cache_timeout"] = 24;
-// css cache file name
+/**
+ * CSS cache file name
+ * @global string $config["css_cache_file"] 
+ */
 $config["css_cache_file"] = "";
 
-// full html cache
-// full path to html cache directory
-// this will store each page in its own file
+/** 
+ * This will store each page in its own file.
+ * full html cache with full path to cache
+ * @global bool $config["html_cache"] 
+ */
 $config["html_cache"] = false; 
 
-// time out for pages cache - in milliseconds
+/** 
+ * Time out for pages cache, in milliseconds
+ * @global integer $config["cache_timeout"] 
+ */
 $config["cache_timeout"] = "86400";
 
-// session life time before auto log out 
-// if no activities from the logged in user - in milliseconds
+/**
+ * Session lifetime before auto log out if no activities from the logged 
+ * in user - in milliseconds
+ * @global integer $config["session_timeout"] 
+ */
 $config["session_timeout"] = 7200;
 
-// if true will allow same username and password to login from two different IPs
+/** 
+ * If true will allow same username and password to login from two different IPs
+ * @global bool $config["allow_multiple_sessions"] 
+ */
 $config["allow_multiple_sessions"] = false;
 
-// register guests sessions
-// for tracking how many users are currently visiting the site
+/**
+ * Register guests sessions for tracking how many users are currently 
+ * visiting the site
+ * @global bool $config["allow_guest_sessions"] 
+ */
 $config["allow_guest_sessions"] = false;
 
-// enable version control system
-// this will store each change for any sql UPDATE command in aiki_revisions
+/**
+ * Enable version control system this will store each change for any sql 
+ * UPDATE command in aiki_revisions
+ * @global bool $config["save_revision_history"] 
+ */
 $config["save_revision_history"] = false;
 
-// store information about not found pages in aiki_redirects
-// so admin can later add redirects
+/** 
+ * Store information about not found pages in aiki_redirects so admin can 
+ * later add redirects
+ * @global bool $config["register_errors"]
+ */
 $config["register_errors"] = false;
 
-// custom 404 error page
+/**
+ * Custom 404 error page
+ * @global string $config["error_404"] 
+ */
 $config["error_404"] = "<h1>404 Page Not Found</h1>
 <p>This page is not found</p>
 <p>Please visit <a href=\"".AIKI_SITE_URL."\">Home page</a> so you may find what you are looking for.</p>"; 
 
-// the default site URL
+/**
+ * The default site URL
+ * @global string $config["url"] 
+ */
 $config["url"] = AIKI_SITE_URL;
 
-// show admin widgets or not?
+/**
+ * Show admin widgets or not?
+ * @global bool $config['admin_widgets_display'] 
+ */
 $config['admin_widgets_display'] = false;
  
-// enable debugging
+/**
+ * Enable debug mode
+ * @global bool $config["debug"] 
+ */
 $config["debug"] = false;
 
-// set the aiki log directory
+/**
+ * Set the aiki log directory
+ * @global string $config["log_dir"]
+ */
 $config["log_dir"] = AIKI_LOG_DIR;
 
-// set the aiki log file name
+/**
+ * Set the aiki log file name
+ * @global string $config["log_file"]
+ */
 $config["log_file"] = AIKI_LOG_FILE;
 
-// set the aiki host profile name
+/**
+ * Set the aiki host profile name
+ * @global string $config["log_profile"]
+ */
 $config["log_profile"] = AIKI_LOG_PROFILE;
 
-// set the aiki log level
+/**
+ * Set the aiki log level
+ * @global string $config["log_level"]
+ */
 $config["log_level"] = AIKI_LOG_LEVEL;
 
 ?>
