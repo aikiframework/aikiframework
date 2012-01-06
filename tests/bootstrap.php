fdae2bf5d65af2ae2b5336b35a5b7d4c96550a98
Jon Phillips <jon@rejon.org>
2012-01-06 18:30:01 +0800

    added text to phpunit and made sure it works. Its important to keep these together. We could include web-based php unit tests later if we wanted

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,12 @@
 
 /** Prepend to the include path for all unit tests.
  * Tests should always prefer the configured and built
- * src before the distributed source. */
+ * src before the distributed source. 
+ *
+ * Add like the following if need in other folders:
+ *
+ * __DIR__ . "/../build/src" . PATH_SEPARATOR .
+ */
 set_include_path(
-    __DIR__ . "/../build/src" . PATH_SEPARATOR .
-    __DIR__ . "/../src" . PATH_SEPARATOR .
     get_include_path()
 );
