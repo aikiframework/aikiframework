d93df51993522b9f953641e81c088ce9f2be5545
Roger Martin <rg1024@gmail.com>
2012-02-04 23:06:37 +0100

    UpgradeDefauls.sql corrected and converted in sql coments

--- sql/UpdateDefaults.sql
+++ sql/UpdateDefaults.sql
@@ -1,31 +1,32 @@
-/* Insert default table records for aiki
+-- Upgrader SQL
 
- * Copyright (C) 2010 Aikilab
+-- Copyright (C) 2010 Aikilab
 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- * Written by Bassel Safadi, Steven Garcia
- * 
- * IMPORTANT in between each pair of SQL statements there must
- * exist a comment line with a precise series of dashes.
- * These are delimiters used by libs/installer.php
- * For more info see SQL_DELIMIT in libs/installer.php.
- *
- *
- * aiki_widget 28 is last id for oficial aiki widget.
- *
- */
+-- IMPORTANT in between each pair of SQL statements there must
+-- exist a comment line with a precise series of dashes.
+
+
+-- Here we delete records that will be inserted by insertDefauls.sql
+-- to ensure overwritten of data. 
+
+-- aiki_dictionary last id is 39
+-- aiki_widget 28 is last id for oficial aiki widget.
+-- aiki_forms id (1,4,6,9,12,13,17,18,20 are oficial
+--
+-- 
 
 -- ------------------------------------------------------
 
@@ -33,10 +34,10 @@ DELETE FROM `aiki_dictionary` WHERE `term_id` < 39;
 
 -- ------------------------------------------------------
 
-DELETE FROM `aiki_forms` WHERE `id` IN (1,4,6,9,12,13,17,18,20)
+DELETE FROM `aiki_forms` WHERE `id` IN (1,4,6,9,12,13,17,18,20);
 
 -- ------------------------------------------------------
 
-DELETE FROM `aiki_widgets` WHERE `id` < 28
+DELETE FROM `aiki_widgets` WHERE `id` < 28;
 
 -- ------------------------------------------------------
